af5db38b1ff0f29594e3de6456303e560c93523f
AmitThakkar <vigildbest@gmail.com>
2016-11-26 16:53:10 +0530

    I N I T I A L I Z I N G

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -8,4 +8,24 @@ import {Component} from '@angular/core';
     templateUrl: './home.html',
 })
 export class HomeComponent {
+    public southIndianItems = [
+        {name: 'Idly', price: 40},
+        {name: 'Vada', price: 50},
+        {name: 'Plain Dosa', price: 50},
+        {name: 'Masala Dosa', price: 80},
+        {name: 'Paneer Dosa', price: 90},
+        {name: 'Onion Uttapam', price: 80}
+    ];
+    public northIndianItems = [
+        {name: 'Samosa', price: 20},
+        {name: 'Poori Aloo Dum', price: 40},
+        {name: 'Aloo Paratha', price: 50},
+        {name: 'Masala Paratha', price: 80},
+        {name: 'Chana Kulcha', price: 80},
+        {name: 'Chola Batura', price: 100}
+    ];
+    public orders = [];
+    public addItem = function () {
+
+    }
 }
